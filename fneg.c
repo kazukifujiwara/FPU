4a932152d168e5989833149dc64c73c16a93f892
kazuki fujiwara <kazuki@kazuki-Latitude-E5410.(none)>
2014-10-19 10:56:06 +0900

    printc.c と共にコンパイル

--- fneg.c
+++ fneg.c
@@ -13,26 +13,13 @@ union data_32bit {
 };
 
 /* デバッグ用　ビット列を表示 */
-void print_32bit(uint32_t n) {
-  int i;
-  int temp = n;
-  for (i = 0; i < 32; i++) {
-    printf("%u", (temp >> (31 - i)) & 1);
-    if (i == 0 || i == 8) {
-      printf(" "); // 1 + 8 + 23;
-    }
-  }
-  printf("\n");
-}
+void print_32bit(uint32_t n);
 
-void print_data(union data_32bit data) {
-  printf("float  : %f\n", data.fl32);
-  printf("uint32 : %u\n", data.uint32);
-  printf("sign   : %u\n", data.sign);
-  printf("exp    : %u\n", data.exp);
-  printf("frac   : %u\n", data.frac);
-  printf("32bit  : "); print_32bit(data.uint32);
-}
+void print_data(union data_32bit data);
+
+char *delete_space(char *str);
+
+uint32_t str_to_uint32(char *str);
 
 uint32_t fneg(uint32_t a) {
   union data_32bit a_32bit;
@@ -42,33 +29,6 @@ uint32_t fneg(uint32_t a) {
 }
 
 #if 0
-char *delete_space(char *str) {
-  char *new = calloc(33, sizeof(char));
-  int i = 0;
-  int j = 0;
-  while (str[i] != '\0') {
-    if (str[i] != ' ') {
-      new[j] = str[i];
-      i++;
-      j++;
-    } else
-      i++;
-  }
-  j++;
-  new[j] = '\0';
-
-  return (new);
-}
-
-uint32_t str_to_uint32(char *str) {
-  int i;
-  uint32_t sum = 0;
-  for (i = 0; i < 32; i++) {
-    if (str[i] == '1')
-      sum = sum + (1 << (31 - i));
-  }
-  return (sum);
-}
 
 int main(void) {
   
